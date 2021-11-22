4893af106e26146c3df1b031b90a741cd53685bc
MilAnos69 <71741789+MilAnos69@users.noreply.github.com>
2021-11-22 13:36:57 -0500

    ayuda jesus

---
+++ src/main/java/cundi/edu/co/demo/PrimerApiApplication.java
@@ -0,0 +1,21 @@
+package cundi.edu.co.demo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class PrimerApiApplication {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerApiApplication.class, args);
+	}
+
+}
