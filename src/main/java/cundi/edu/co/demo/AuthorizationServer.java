d903ec86c9acf4a50a402edd4573f3766358c868
MilAnos69 <71741789+MilAnos69@users.noreply.github.com>
2021-11-24 00:15:41 -0500

    15 min

--- src/main/java/cundi/edu/co/demo/AuthorizationServer.java
+++ src/main/java/cundi/edu/co/demo/AuthorizationServer.java
@@ -58,7 +58,7 @@ public class AuthorizationServer  extends AuthorizationServerConfigurerAdapter{
 	@Override
 	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
 		configurer.inMemory().withClient(clientId).secret(bcrypt.encode(clientSecret)).authorizedGrantTypes(grantType)
-		.scopes(scopeRead, scopeWrite).resourceIds(resourceIds).accessTokenValiditySeconds(600)
+		.scopes(scopeRead, scopeWrite).resourceIds(resourceIds).accessTokenValiditySeconds(900)
 		.refreshTokenValiditySeconds(0);
 	}
 
