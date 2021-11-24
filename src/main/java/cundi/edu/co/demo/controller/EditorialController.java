c7d53a55376c18a292c7467faaca8af0f655e939
MilAnos69 <71741789+MilAnos69@users.noreply.github.com>
2021-11-24 11:24:19 -0500

    seguridad2

--- src/main/java/cundi/edu/co/demo/controller/EditorialController.java
+++ src/main/java/cundi/edu/co/demo/controller/EditorialController.java
@@ -32,7 +32,7 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-@PreAuthorize("hasAuthority('Vendedor')")
+//@PreAuthorize("hasAuthority('Vendedor')")
 @RestController
 @RequestMapping("editorial")
 @Validated
